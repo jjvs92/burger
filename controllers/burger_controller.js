43f873032dcde03ece3af720a9efad0ba482bed6
jjvs92 <julian_jvs92@hotmail.com>
2018-07-13 08:39:02 -0500

    almost fully functioning app

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res){
         var hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
+        // console.log(hbsObject);
 
         res.render("index", hbsObject);
     })
@@ -18,11 +18,28 @@ router.get("/", function(req, res){
 
 router.post("/api/burgers", function(req, res){
     burger.create([
-        "name", "devoured"
+        "burger_name", "devoured"
     ],[req.body.name, req.body.devoured],
     function(result){
         res.json({ id: result.inserId});
     });
 });
+
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;   
+     
+    burger.udpate({
+        devoured: req.body.devoured},
+        condition, function(result){
+            if (result.changedRows == 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+              } else {
+                res.status(200).end();
+            }            
+        }
+
+    )
+})
 
 module.exports = router;
